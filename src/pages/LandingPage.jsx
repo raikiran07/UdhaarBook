fd1d529016b3632aa9d046180a8da467f41180ff
raikiran07 <raikiran925@gmail.com>
2024-06-23 15:21:56 +0530

    landing page updated

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -16,10 +16,10 @@ const LandingPage = () => {
         <h2 className='text-white text-center mt-24 text-2xl'>Features and Process</h2>
         <hr className='max-w-[30px] mx-auto mt-2' />
         <div className='flex flex-col items-center justify-center gap-4 max-w-[60%] mx-auto mt-4 ease-linear md:flex-row'>
-            <div className=' text-gray-800 border p-2 bg-gray-300 rounded-md min-h-[260px]'>
+            <div className=' text-gray-800 border p-2 bg-gray-300 rounded-md min-h-[260px] flex-1 hover:scale-150 duration-100 ease-linear'>
             <h1 className='text-2xl bg-gray-800 text-white max-w-[50px] min-h-[50px] rounded-full flex items-center justify-center'>1</h1>
             <p className='text-lg tracking-wide min-h-32 mt-2'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dicta tempora excepturi perspiciatis? Aliquam labore voluptas consequuntur aspernatur molestiae est?
+            An easy sign-in method involves using Single Sign-On (SSO)  to streamline access and enhance security.
 
             </p>
             <Link to="/login">
@@ -27,20 +27,20 @@ const LandingPage = () => {
             </Link>
             
             </div>
-            <div className='card text-white border p-2 rounded-md hover:bg-gray-300 hover:text-gray-800 min-h-[260px]'>
+            <div className='card text-white border p-2 rounded-md hover:bg-gray-300 hover:text-gray-800 min-h-[260px] flex-1 hover:scale-150 duration-100 ease-linear'>
             <h1 className='text-2xl border max-w-[50px] min-h-[50px] rounded-full flex items-center justify-center text-white'>2</h1>
             <p className='text-lg tracking-wide min-h-32 mt-2'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dicta tempora excepturi perspiciatis? Aliquam labore voluptas consequuntur aspernatur molestiae est?
+            An easy sign-up method uses  email verification to quickly create accounts with minimal user input and maximum convenience.
             </p>
             <Link to="/register">
             <span className='underline text-blue-700'>singUp</span>
             </Link>
             
             </div>
-            <div className='card text-white border p-2 rounded-md hover:bg-gray-300 hover:text-gray-800 min-h-[270px]'>
+            <div className='card text-white border p-2 rounded-md hover:bg-gray-300 hover:text-gray-800 min-h-[260px] flex-1 hover:scale-150 duration-100 ease-linear'>
             <h1 className='text-2xl border max-w-[50px] min-h-[50px] rounded-full flex items-center justify-center text-white ease-linear '>3</h1>
             <p className='text-lg tracking-wide min-h-32 mt-2'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dicta tempora excepturi perspiciatis? Aliquam labore voluptas consequuntur aspernatur molestiae est?
+            udhaarbook provides an easy udhaar management system for seamless tracking and managing of debts.
             </p>
           
             
